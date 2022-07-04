279e6c2721f928973418ab4fb44d423a30952d91
Deby2404 <dedede2404@gmail.com>
2022-07-04 09:14:18 +0900

    [add]switch practice

--- conditionals-if-and-switch/src/com/in28minutes/ifstatement/examples/SwitchExercisesRunner.java
+++ conditionals-if-and-switch/src/com/in28minutes/ifstatement/examples/SwitchExercisesRunner.java
@@ -5,19 +5,26 @@ public class SwitchExercisesRunner {
 	public static void main(String[] args) {
 		System.out.println(determineNameOfDay(0));
 	}
-	
+
 	public static boolean isWeekDay(int dayNumber) {
-		switch(dayNumber) {
-		case 0: return false;
-		case 1: return true;
-		case 2: return true;
-		case 3: return true;
-		case 4: return true;
-		case 5: return true;
-		case 6: return false;
+		switch (dayNumber) {
+		case 0:
+			return false;
+		case 1:
+			return true;
+		case 2:
+			return true;
+		case 3:
+			return true;
+		case 4:
+			return true;
+		case 5:
+			return true;
+		case 6:
+			return false;
 		}
 		return false;
-		
+
 	}
 
 	public static String determineNameOfDay(int dayNumber) {
