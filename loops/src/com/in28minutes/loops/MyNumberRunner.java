3442b0bafee4a87ae63fa41e7acdb51f45515376
Deby2404 <dedede2404@gmail.com>
2022-07-15 21:36:35 +0900

    sum and division

--- loops/src/com/in28minutes/loops/MyNumberRunner.java
+++ loops/src/com/in28minutes/loops/MyNumberRunner.java
@@ -3,9 +3,16 @@ package com.in28minutes.loops;
 public class MyNumberRunner {
 
 	public static void main(String[] args) {
-		MyNumber number = new MyNumber(3);
+		MyNumber number = new MyNumber(9);
+		
 		boolean isPrime = number.isPrime();
 		System.out.println("isPrime " + isPrime);
+		
+		int sum  = number.sumUptoN();
+		System.out.println("sumUptoN" + sum);
+		
+		int sumOfDivisors = number.sumOfDivisors();
+		System.out.println("sumOfDivisors" + sum);
 
 	}
 
